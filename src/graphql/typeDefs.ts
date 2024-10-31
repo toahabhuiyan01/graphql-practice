e2a605ac32fff2d72448a1f879fb9a754762dd59
Toaha Bhuiyan <67577253+toahabhuiyan01@users.noreply.github.com>
2024-10-31 19:42:12 +0600

    chore

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -22,6 +22,8 @@ export const typeDefs = `
 
     type Query {
         users: [User]
+
+        """This Needs Authorization"""
         products(
             category: String,
             minPrice: Float,
@@ -29,12 +31,16 @@ export const typeDefs = `
             limit: Int = 10,
             offset: Int = 0
         ): [Product]
+
+        """This Needs Authorization"""
         orders(
             productId: String,
             limit: Int = 10,
             offset: Int = 0
         ): [Order]
+
         topRankingUsers: [User]
+        
         totalSalesPerCategory: [CategorySales]
     }
 
@@ -52,13 +58,26 @@ export const typeDefs = `
     }
 
     type Mutation {
+        """This Needs Authorization"""
         createUser(username: String!, password: String!): User
+
+        """This Needs Authorization"""
         updateUser(username: String, password: String): Success
+
         login(username: String!, password: String!): AccessToken
+
+        """This Needs Authorization"""
         createProduct(name: String!, category: String!, price: Float!): Product
+
+        """This Needs Authorization"""
         updateProduct(id: ID!, name: String, category: String, price: Float): Product
+
         createOrder(productId: ID!, quantity: Int!): Order
+
+        """This Needs Authorization"""
         updateOrder(id: ID!, status: String): Success
+
+        """This Needs Authorization"""
         deleteProduct(id: ID!): Boolean
     }
 `
